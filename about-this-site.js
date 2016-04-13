887cece486fa6a1b5d80222a073757276e54a216
Hua <liaoweihua.real@gmail.com>
2016-04-13 23:01:17 +0800

    Update about-this-site.js

--- about-this-site.js
+++ about-this-site.js
@@ -1,5 +1,5 @@
 module.exports = {
     DomainName: "你的DomainName",
-    AnnoyPostUrl: "使用者想發文的url",
-    LoginReportUrl: "使用者想檢舉的url"
+    AnnoyPostUrl: "使用者想發文的url",      //範例: http://toolman.kaobei.ga/
+    LoginReportUrl: "使用者想檢舉的url"     //範例: http://toolman.kaobei.ga/report/    最後一個字元必須是斜線
 };
